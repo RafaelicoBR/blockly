9bce00fbc0f1f202c58f236f75ccbf6566da41b5
neil.fraser@gmail.com <neil.fraser@gmail.com@c400ca83-b69d-9dd7-9705-49c6b8615e23>
2013-07-16 18:47:16 +0000

    Removing old i18n branch.

--- branches/i18n/apps/en.js
+++
@@ -1,9 +0,0 @@
-// This file was automatically generated from template.soy.
-// Please don't edit this file by hand.
-
-if (typeof BlocklyCommonMessages == 'undefined') { var BlocklyCommonMessages = {}; }
-
-
-BlocklyCommonMessages.messages = function(opt_data, opt_ignored) {
-  return '<script>if (typeof Blockly == \'undefined\') {var Blockly = {};}if (typeof Blockly.Storage == \'undefined\') {Blockly.Storage = {};}Blockly.Storage.HTTPREQUEST_ERROR = "There was a problem with the request. %1"; Blockly.Storage.LINK_ALERT = "Share your blocks with this link: %1"; Blockly.Storage.HASH_ERROR = "Sorry, \'%1\' doesn\'t correspond with any saved Blockly file."; Blockly.Storage.XML_ERROR = "Could not load your saved file.  Perhaps it was created with a different version of Blockly?" ;<\/script>';
-};
