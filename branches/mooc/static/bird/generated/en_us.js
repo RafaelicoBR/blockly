cf8f7708c27052aa74cd0802dbd8ffe2855ca6e0
spertus@google.com <spertus@google.com@c400ca83-b69d-9dd7-9705-49c6b8615e23>
2013-08-28 23:05:08 +0000

    Fixes to i18n for en->en_us and addition of common.soy. Addition of bird. i18n/xliff_to_json.py now removes .xlf input file.
    R=neil.fraser@gmail.com
    
    Review URL: https://codereview.appspot.com/13324044

---
+++ branches/mooc/static/bird/generated/en_us.js
@@ -0,0 +1,60 @@
+// This file was automatically generated from common.soy.
+// Please don't edit this file by hand.
+
+if (typeof apps == 'undefined') { var apps = {}; }
+
+
+apps.messages = function(opt_data, opt_ignored, opt_ijData) {
+  return '<div style="display: none"><span id="subtitle">a visual programming environment</span><span id="blocklyMessage">Blockly</span><span id="codeTooltip">See generated JavaScript code. </span><span id="linkTooltip">Save and link to blocks.</span><span id="runTooltip">Run the program defined by the blocks in the \\nworkspace. </span><span id="runProgram">Run Program</span><span id="resetProgram">Reset</span><span id="dialogOk">OK</span><span id="dialogCancel">Cancel</span><span id="catLogic">Logic</span><span id="catLoops">Loops</span><span id="catMath">Math</span><span id="catText">Text</span><span id="catLists">Lists</span><span id="catColour">Colour</span><span id="catVariables">Variables</span><span id="catProcedures">Procedures</span><span id="httpRequestError">There was a problem with the request.</span><span id="linkAlert">Share your blocks with this link:\n\n%1</span><span id="hashError">Sorry, \'%1\' doesn\'t correspond with any saved program.</span><span id="xmlError">Could not load your saved file.  Perhaps it was created with a different version of Blockly?</span><span id="listVariable">list</span><span id="textVariable">text</span></div>';
+};
+
+
+apps.dialog = function(opt_data, opt_ignored, opt_ijData) {
+  return '<div id="dialogShadow" class="dialogAnimate"></div><div id="dialogBorder"></div><div id="dialog"></div>';
+};
+
+
+apps.codeDialog = function(opt_data, opt_ignored, opt_ijData) {
+  return '<div id="dialogCode" class="dialogHiddenContent"><pre id="containerCode"></pre>' + apps.ok(null, null, opt_ijData) + '</div>';
+};
+
+
+apps.storageDialog = function(opt_data, opt_ignored, opt_ijData) {
+  return '<div id="dialogStorage" class="dialogHiddenContent"><div id="containerStorage"></div>' + apps.ok(null, null, opt_ijData) + '</div>';
+};
+
+
+apps.ok = function(opt_data, opt_ignored, opt_ijData) {
+  return '<div class="farSide" style="padding: 1ex 3ex 0"><button class="secondary" onclick="BlocklyApps.hideDialog(true)">OK</button></div>';
+};
+
+;
+// This file was automatically generated from template.soy.
+// Please don't edit this file by hand.
+
+if (typeof birdpage == 'undefined') { var birdpage = {}; }
+
+
+birdpage.messages = function(opt_data, opt_ignored, opt_ijData) {
+  return apps.messages(null, null, opt_ijData) + '<div style="display: none"><span id="Bird_hungry">hungry</span><span id="Bird_heading">heading</span><span id="Bird_if">if</span><span id="Bird_doCode">do</span><span id="Bird_elseCode">else</span><span id="Bird_hungryTooltip">The condition when the bird has not gotten the \\nfood. </span><span id="Bird_headingTooltip">Move in the direction of the given angle: 0 is \\nto the right, 90 is straight up, etc. </span><span id="Bird_positionTooltip">x and y mark the bird\'s position. \\nWhen x = 0 the bird is near the left \\nedge, when x = 100 it\'s near the \\nright edge. When y = 0 the bird is \\nat the bottom, when y = 100 it\'s at \\nthe top. </span><span id="Bird_nextLevel">Congratulations! Are you ready to proceed to level %1?</span><span id="Bird_finalLevel">Congratulations! You have solved the final level.</span></div>';
+};
+
+
+birdpage.start = function(opt_data, opt_ignored, opt_ijData) {
+  var output = birdpage.messages(null, null, opt_ijData) + '<table width="100%"><tr><td><h1><span id="title"><a href="../index.html">Blockly</a> : Bird</span> &nbsp; ';
+  for (var i131 = 1; i131 < 11; i131++) {
+    output += ' ' + ((i131 == opt_ijData.level) ? '<span class="tab" id="selected">' + soy.$$escapeHtml(i131) + '</span>' : (i131 < opt_ijData.level) ? '<a class="tab previous" href="?lang=' + soy.$$escapeHtml(opt_ijData.lang) + '&level=' + soy.$$escapeHtml(i131) + '">' + soy.$$escapeHtml(i131) + '</a>' : '<a class="tab" href="?lang=' + soy.$$escapeHtml(opt_ijData.lang) + '&level=' + soy.$$escapeHtml(i131) + '">' + soy.$$escapeHtml(i131) + '</a>');
+  }
+  output += '</h1></td><td class="farSide"><select id="languageMenu" onchange="BlocklyApps.changeLanguage();"></select></td></tr></table><div id="visualization"><svg xmlns="http://www.w3.org/2000/svg" version="1.1" id="svgBird" width="400px" height="400px"></svg></div><table width="400"><tr><td style="width: 190px; text-align: center; vertical-align: top;"><button class="notext" title="See generated JavaScript code. " onclick="BlocklyApps.showCode(this);"><img src="../media/1x1.gif" class="code icon21"></button><button id="linkButton" class="notext" title="Save and link to blocks." onclick="BlocklyStorage.link();"><img src="../media/1x1.gif" class="link icon21"></button></td><td><button id="runButton" class="primary" onclick="Bird.runButtonClick();" title="Makes the player do what the blocks say."><img src="../media/1x1.gif" class="run icon21"> Run Program</button><button id="resetButton" class="primary" onclick="Bird.resetButtonClick();" style="display: none" title="Put the player back at the start of the level."><img src="../media/1x1.gif" class="stop icon21"> Reset</button></td></tr></table><script type="text/javascript" src="../blockly_compressed.js"><\/script><script type="text/javascript" src="../javascript_compressed.js"><\/script><script type="text/javascript" src="../' + soy.$$escapeHtml(opt_ijData.langSrc) + '"><\/script><script type="text/javascript" src="blocks.js"><\/script>' + birdpage.toolbox(null, null, opt_ijData) + '<div id="blockly"></div>' + apps.dialog(null, null, opt_ijData) + apps.codeDialog(null, null, opt_ijData) + apps.storageDialog(null, null, opt_ijData) + '<div id="dialogDone" class="dialogHiddenContent"><div id="dialogDoneText" style="font-size: large; margin: 1em;"></div><div id="dialogDoneButtons" class="farSide" style="padding: 1ex 3ex 0"></div></div>';
+  return output;
+};
+
+
+birdpage.toolbox = function(opt_data, opt_ignored, opt_ijData) {
+  return '<xml id="toolbox" style="display: none;"><block type="bird_heading"></block>' + ((opt_ijData.level >= 2) ? '<block type="bird_hungry"></block>' + ((opt_ijData.level >= 4) ? '<block type="bird_compare"><title name="OP">LT</title><value name="A"><block type="bird_position"><title name="XY">X</title></block></value><value name="B"><block type="math_number"><title name="NUM">50</title></block></value></block>' + ((opt_ijData.level >= 5) ? '<block type="bird_compare"><title name="OP">LT</title><value name="A"><block type="bird_position"><title name="XY">Y</title></block></value><value name="B"><block type="math_number"><title name="NUM">50</title></block></value></block>' + ((opt_ijData.level >= 8) ? '<block type="bird_and"></block>' : '') : '') : '') : '') + '</xml>';
+};
+
+
+birdpage.readonly = function(opt_data, opt_ignored, opt_ijData) {
+  return birdpage.messages(null, null, opt_ijData) + '<script type="text/javascript" src="../../blockly_compressed.js"><\/script><script type="text/javascript" src="../../' + soy.$$escapeHtml(opt_ijData.langSrc) + '"><\/script><script type="text/javascript" src="blocks.js"><\/script><div id="blockly"></div>';
+};
